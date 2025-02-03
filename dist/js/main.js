30d12d5185e4c577e4e633f4d9c81d7463ef51a7
max Ambrogio <86671808+Max-Ambrogio@users.noreply.github.com>
2025-02-03 17:56:21 -0500

    prepare-for da pixels

--- dist/js/main.js
+++ dist/js/main.js
@@ -90,83 +90,6 @@ if (document.body.classList.contains('page-home')) {
       return Math.random() * (max - min) + min;
     }
 
-    const info = [
-        {"text": "Web Development" },
-        {"text": "2D / 3D Animation" },
-        {"text": "Video Production" },
-        {"text": "Creative Technology" },
-      ]
-      
-      //duplicate cards
-    //   const grid = document.querySelector(".cards")
-    //   const card = document.querySelector(".card")
-      
-    //   for (let i = 1; i < info.length; i++) {
-    //     const newCard = card.cloneNode(true);
-    //     grid.appendChild(newCard)
-    //   }
-      
-      var cards = document.querySelectorAll('.card')
-      
-    //   cards.forEach((item, i) => {
-    //     item.querySelector('h1').innerHTML = Number(i + 1)
-    //     item.querySelector('p').innerHTML = info[i].text
-    //   })
-      
-      const cursorCont = document.querySelector(".cursor-container")
-      const cursor = document.querySelector(".cursor")
-      const root = document.documentElement;
-      
-      document.addEventListener("mousemove", (event) => {
-            x = event.pageX
-          y = event.pageY
-        
-          cursorCont.style.transform = `translate(${x}px,${y}px) scale(1)`
-      })
-      
-      //hover
-      var cardInner = document.querySelectorAll('.card .inner-shadow')
-      var cardCursorWrapper = document.querySelectorAll('.card .cursor-blur-wrapper')
-      var cardCursor = document.querySelectorAll('.card .cursor-blur')
-      
-      window.addEventListener("mousemove", (event) => {
-          cards.forEach((item, i) => {
-            const rect = item.getBoundingClientRect();
-          let x = event.clientX - rect.left;
-          let y = event.clientY - rect.top;
-          let count = 0;
-          
-          if (x < 0) {
-              x = 0
-            translate()
-          } else if (x > rect.width) {
-              x = rect.width
-            translate()
-          }
-          if (y < 0) {
-              y = 0
-            translate()
-          } else if (y > rect.height) {
-              y = rect.height
-            translate()
-          }
-          function translate() {
-              cardCursorWrapper[i].style.transform = `translate(${x}px,${y}px)`
-          }
-          
-          item.onmouseover = function(){
-            cursor.style.backgroundColor = `var(--animate${i + 1})`
-            cardCursor[i].style.backgroundColor = `var(--animate${i + 1})`
-            cardInner[i].style.color = `var(--animate${i + 1})`
-          };
-          item.onmouseout = function(){
-            cursor.style.backgroundColor = 'var(--initial)'
-            cardCursor[i].style.backgroundColor = 'var(--initial)'
-            cardInner[i].style.color = 'var(--initial)'
-          };  
-        })
-      })
-
     gsap.to('.scramble',{
         duration:2,
         autoAlpha: 0,
@@ -225,6 +148,63 @@ if (document.body.classList.contains('page-home')) {
         duration:3,
         stagger:1,
     })
+      
+      var cards = document.querySelectorAll('.card')
+      
+      const cursorCont = document.querySelector(".cursor-container")
+      const cursor = document.querySelector(".cursor")
+      const root = document.documentElement;
+      
+      document.addEventListener("mousemove", (event) => {
+            x = event.pageX
+          y = event.pageY
+        
+          cursorCont.style.transform = `translate(${x}px,${y}px) scale(1)`
+      })
+      
+      //hover
+      var cardInner = document.querySelectorAll('.card .inner-shadow')
+      var cardCursorWrapper = document.querySelectorAll('.card .cursor-blur-wrapper')
+      var cardCursor = document.querySelectorAll('.card .cursor-blur')
+      
+      window.addEventListener("mousemove", (event) => {
+          cards.forEach((item, i) => {
+            const rect = item.getBoundingClientRect();
+          let x = event.clientX - rect.left;
+          let y = event.clientY - rect.top;
+          let count = 0;
+          
+          if (x < 0) {
+              x = 0
+            translate()
+          } else if (x > rect.width) {
+              x = rect.width
+            translate()
+          }
+          if (y < 0) {
+              y = 0
+            translate()
+          } else if (y > rect.height) {
+              y = rect.height
+            translate()
+          }
+          function translate() {
+              cardCursorWrapper[i].style.transform = `translate(${x}px,${y}px)`
+          }
+          
+          item.onmouseover = function(){
+            cursor.style.backgroundColor = `var(--animate${i + 1})`
+            cardCursor[i].style.backgroundColor = `var(--animate${i + 1})`
+            cardInner[i].style.color = `var(--animate${i + 1})`
+          };
+          item.onmouseout = function(){
+            cursor.style.backgroundColor = 'var(--initial)'
+            cardCursor[i].style.backgroundColor = 'var(--initial)'
+            cardInner[i].style.color = 'var(--initial)'
+          };  
+        })
+      });
+
 }
 
 
